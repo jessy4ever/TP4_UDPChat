4521a611b29f08bc8122869f705a89f9bb8f6601
Yasmine Abbes <yasminabbes473@gmail.com>
2025-11-15 18:31:32 +0100

    activite tp4: chat bidirectionnel et multi clients

--- src/udpchat/ClientUDP.java
+++ src/udpchat/ClientUDP.java
@@ -7,15 +7,29 @@ public class ClientUDP {
     public static void main(String[] args) {
         try {
             Scanner sc = new Scanner(System.in);
-            System.out.print("Entrez votre nom d'utilisateur : ");
+            System.out.print("Nom d'utilisateur : ");
             String username = sc.nextLine();
 
             DatagramSocket socket = new DatagramSocket();
             InetAddress serverAddr = InetAddress.getByName("localhost");
             int serverPort = 1234;
 
-            System.out.println("Tapez vos messages (ou 'quit' pour quitter):");
+            Thread receiveThread = new Thread(() -> {
+                byte[] buffer = new byte[1024];
+                while (true) {
+                    try {
+                        DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+                        socket.receive(packet);
+                        String msg = new String(packet.getData(), 0, packet.getLength());
+                        System.out.println(msg);
+                    } catch (Exception e) {
+                        break;
+                    }
+                }
+            });
+            receiveThread.start();
 
+            System.out.println("Tapez vos messages ('quit' pour quitter)");
             while (true) {
                 String msg = sc.nextLine();
                 if (msg.equalsIgnoreCase("quit")) break;
@@ -28,7 +42,7 @@ public class ClientUDP {
 
             socket.close();
             sc.close();
-            System.out.println("Client terminé.");
+            System.out.println("Déconnexion...");
         } catch (Exception e) {
             e.printStackTrace();
         }
